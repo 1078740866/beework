f3fc01d1e7f271d2dad697b135ef5a15a8516ba4
kan <1078740866@qq.com>
2022-07-15 15:07:00 +0900

    components-model

---
+++ componentsHomework/src/components/Child.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <button :class="btnStyle" @click="send"><slot></slot></button><br />
+  </div>
+</template>
+
+<script>
+export default {
+   props: {
+      btnStyle: String
+    },
+  data () {
+    return {
+      allMessage: [
+        { message:"保存しました。"},
+        { message2:"キャンセルしました。"}
+      ],
+    
+  }
+  },
+  methods: {
+    send() {
+      this.$emit("click", this.allMessage);
+    },
+  
+  },
+};
+</script>
+<style scoped>
+.save {
+  background-color: rgb(246, 0, 0);
+  color: rgb(255, 255, 255);
+  width: 150px;
+  height: 30px;
+}
+.cancel {
+  background-color: rgb(0, 50, 249);
+  color: azure;
+  width: 150px;
+  height: 30px;
+}
+.input {
+  margin-left: 10px;
+  height: 38px;
+  width: 100px;
+  font-size: 30px;
+}
+.btn {
+  height: 40px;
+  width: 100px;
+  font-size: 20px;
+  background-color: rgba(255, 217, 113, 0.76);
+  border: 2px solid #ccc;
+}
+.btn:hover {
+  cursor: pointer;
+  background-color: yellow;
+}
+p {
+  font-size: 30px;
+}
+</style>
---
+++ componentsHomework/src/components/Kingaku.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- <Child @click="getMessage" /> -->
+    <Child btnStyle="save" @click="getMessage">保存</Child><br><br>
+    <Child btnStyle="cancel" @click="getMessage2">キャンセル</Child>
+    <p class="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import Child from "./Child.vue";
+export default {
+  data() {
+    return {
+      message:"",
+    };
+  },
+  components: {
+    Child,
+  },
+
+  methods: {
+    getMessage() {
+      this.message="保存しました"
+    },
+    getMessage2() {
+      this.message="キャンセルしました"
+    },
+  },
+};
+</script>
+<style>
+p {
+  font-size: 40px;
+}
+.message {
+  background-color: rgb(225, 246, 250);
+  font: 100;
+}
+</style>
